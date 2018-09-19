60eab5a5b09b5ca1c692595e7146cd9d71bec233
Simon Matic Langford <simon@exemel.co.uk>
2018-09-19 20:23:50 +0100

    Update runPrChecks.sh

--- runPrChecks.sh
+++ runPrChecks.sh
@@ -5,4 +5,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
         -d "{\"body\": \"Hello world\"}" \
         "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+else
+   echo "Not a PR"
 fi
